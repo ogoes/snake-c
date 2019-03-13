45e7f0e2646a08f915dbd05d80ce7f99eb684d49
ogoes <otaviosilva820@gmail.com>
2019-03-13 12:01:31 -0300

    Show Terminal Size in Specific Row

--- screen/screen.c
+++ screen/screen.c
@@ -3,22 +3,24 @@
 
 
 /**
+ * show_terminal_size()
  ** show in line the number of columns and rows of the terminal
+ * @param: specific_row -> specific row to print the information about the terminal size
  **/
-void show_terminal_size () {
+void show_terminal_size (unsigned int specific_row) {
   
   /**
-   *! TERMINAL_WINDOW_SIZE hasn't been initialized error
+   *! TERMINAL_WINDOW_SIZE wasn't been initialized, error
    **/
-  // if (!TERMINAL_WINDOW_SIZE) {
-  //   perror("TERMINAL_WINDOW_SIZE hasn't been initialized, call the initialize() function");
-  //   return;
-  // }
+  if (!HAS_BEEN_INITIALIZATED) {
+    perror("TERMINAL_WINDOW_SIZE wasn't been initialized");
+    return;
+  }
 
   unsigned int columns_size = TERMINAL_WINDOW_SIZE.ws_col;
 
   //! 1234 cols/1234 rows - pattern (19 chars)
-  int pattern_chars = 19;
+  int pattern_chars = 17;
   unsigned int blank_chars = columns_size - pattern_chars;
 
 
@@ -27,24 +29,10 @@ void show_terminal_size () {
    * move cursor to daterminated position
    ** "\33[line;columnsH"
    */
-  printf("\33[0;%uH", blank_chars);
+
+  //* Using a specific row to print the information
+  printf("\33[%u;%uH", specific_row, blank_chars);
 
   // Show the terminal informations
-  printf("%4u rows/%4u cols", TERMINAL_WINDOW_SIZE.ws_row, TERMINAL_WINDOW_SIZE.ws_col);
-}
-
-int main () {
-
-  /**
-   * ioctl
-   ** The ioctl() system call manipulates the underlying device parameters of special files.
-   ** see in http://man7.org/linux/man-pages/man2/ioctl.2.html
-   * Init
-   */
-
-
-  ioctl(0, TIOCGWINSZ, &TERMINAL_WINDOW_SIZE);
-  show_terminal_size();
-  
-  return 0;
+  printf("%3u rows/%3u cols\n", TERMINAL_WINDOW_SIZE.ws_row, TERMINAL_WINDOW_SIZE.ws_col);
 }
--- screen/screen.h
+++ screen/screen.h
@@ -6,13 +6,16 @@
  ** global variable that contains terminal width and height information
  **/
 struct winsize TERMINAL_WINDOW_SIZE;
+int HAS_BEEN_INITIALIZATED = 0;
 
 
 
 /**
+ * show_terminal_size()
  ** show in line the number of columns and rows of the terminal
+ * @param: specific_row -> specific row to print the information about the terminal size
  **/
-void show_terminal_size ();
+void show_terminal_size (unsigned int);
 
 
 #endif
